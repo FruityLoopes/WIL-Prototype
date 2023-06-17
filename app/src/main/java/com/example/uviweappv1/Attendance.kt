da1d90a3576cd7a51b17645f995c860334fffd98
ZwangaMJ <ST10085086@vcconnect.edu.za>
2023-06-17 14:39:32 +0200

    Updated the Attandance layout

--- app/src/main/java/com/example/uviweappv1/Attendance.kt
+++ app/src/main/java/com/example/uviweappv1/Attendance.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.util.Calendar
@@ -20,6 +21,7 @@ class Attendance : AppCompatActivity() {
 
     private lateinit var dateRecyclerView: RecyclerView
     private lateinit var dateAdapter: DateAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_attendance)
@@ -66,12 +68,12 @@ class Attendance : AppCompatActivity() {
                 LayoutInflater.from(parent.context).inflate(R.layout.item_date, parent, false)
             return DateViewHolder(view)
         }
-
+        private var selectedItemPosition = RecyclerView.NO_POSITION
         override fun onBindViewHolder(holder: DateAdapter.DateViewHolder, position: Int) {
             val date = dateList[position]
             val dateFormat = SimpleDateFormat("MMM dd", Locale.getDefault())
             val formattedDate = dateFormat.format(date)
-            holder.bind(formattedDate)
+            holder.bind(formattedDate, position == selectedItemPosition)
         }
 
         override fun getItemCount(): Int {
@@ -82,12 +84,28 @@ class Attendance : AppCompatActivity() {
 
             private val dateTextView: TextView = itemView.findViewById(R.id.dateTextView)
 
-            fun bind(date: String) {
+            fun bind(date: String, isSelected: Boolean) {
                 dateTextView.text = date
+                if (isSelected) {
+                    // Apply highlighting to the selected date
+                    itemView.setBackgroundResource(R.drawable.selected_date_item_background)
+                    dateTextView.setTextColor(ContextCompat.getColor(itemView.context, R.color.selected_date_text_color))
+                } else {
+                    // Reset background and text color for unselected dates
+                    itemView.setBackgroundResource(0)
+                    dateTextView.setTextColor(ContextCompat.getColor(itemView.context, R.color.date_text_color))
+                }
+
+
                 // Add click listener to handle item click
                 itemView.setOnClickListener {
                     val clickedDate = dateList[adapterPosition]
+                    val previousSelectedPosition = selectedItemPosition
+                    selectedItemPosition = adapterPosition
+                    notifyItemChanged(previousSelectedPosition)
+                    notifyItemChanged(selectedItemPosition)
                     // Handle the click event for the selected date
+
                     Toast.makeText(
                         itemView.context,
                         "Clicked date: $clickedDate",
