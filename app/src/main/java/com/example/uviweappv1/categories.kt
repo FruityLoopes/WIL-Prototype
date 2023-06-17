bf02c3e80f74d504f23221caf13d168647aa0d5b
ZwangaMJ <ST10085086@vcconnect.edu.za>
2023-06-17 09:47:56 +0200

    Added a category choice layout

---
+++ app/src/main/java/com/example/uviweappv1/categories.kt
@@ -0,0 +1,11 @@
+package com.example.uviweappv1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class categories : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categories)
+    }
+}
