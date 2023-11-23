ea6500034ad92a3701b0098ff2368a7c4a251ded
Chad Andrews <chadandrews07@gmail.com>
2023-11-23 21:32:33 +0200

    Fixed Donation now stored in firebase, and Attendance is now stored in firebase. Made look of some layouts better

--- app/src/main/java/uviwe/app/uviweappv1/Attendance.kt
+++ app/src/main/java/uviwe/app/uviweappv1/Attendance.kt
@@ -1,20 +1,17 @@
 package uviwe.app.uviweappv1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.uviweappv1.R
@@ -24,10 +21,10 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.values
 import com.google.firebase.ktx.Firebase
-import java.util.Calendar
-import java.util.Date
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Locale
 import java.util.concurrent.Executors
 
@@ -36,17 +33,16 @@ class Attendance : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     lateinit var attAdapter: AttendanceAdapter
     private val Items: MutableList<AttendanceData> = mutableListOf()
-    private lateinit var dateRecyclerView: RecyclerView
-    private lateinit var dateAdapter: DateAdapter
+    private lateinit var dateAtt: TextView
     private lateinit var spinClass: Spinner
+    private var spinnerValue : String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_attendance)
 
         auth = Firebase.auth
 
-        dateRecyclerView = findViewById(R.id.dateRecyclerView)
-        dateAdapter = DateAdapter()
+        dateAtt = findViewById(R.id.dateAtt)
         spinClass = findViewById(R.id.spClass)
 
         val listClass : ArrayList<String> = ArrayList()
@@ -62,25 +58,18 @@ class Attendance : AppCompatActivity() {
             layoutManager = LinearLayoutManager(context)
             adapter = attAdapter
         }
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        dateRecyclerView.layoutManager = layoutManager
-        dateRecyclerView.adapter = dateAdapter
-
         // Populate the date list with the desired range of dates
-        val startDate = Calendar.getInstance()
-        startDate.add(Calendar.DAY_OF_MONTH, -7) // Start from 7 days ago
+        val c = Calendar.getInstance().time
 
-        val endDate = Calendar.getInstance()
+        val df = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+        val fb = SimpleDateFormat("yyyyMMdd" ,  Locale.getDefault())
+        val formattedDate = df.format(c)
+        val formattedFB = fb.format(c)
 
-        val dateList = ArrayList<Date>()
 
 
-        while (startDate <= endDate) {
-            dateList.add(startDate.time)
-            startDate.add(Calendar.DAY_OF_MONTH, 1)
-        }
+        dateAtt.text = formattedDate.toString()
 
-        dateAdapter.setDateList(dateList)
 
         val database =
             FirebaseDatabase.getInstance("https://wil-prototype-default-rtdb.europe-west1.firebasedatabase.app/")
@@ -90,7 +79,7 @@ class Attendance : AppCompatActivity() {
         myRef.addValueEventListener(object: ValueEventListener { //must be adjusted to timesheet values
 
             override fun onDataChange(snapshot: DataSnapshot) {
-
+                listClass.clear()
                 for(keys in snapshot.children){
                     listClass.add(keys.key.toString())
                 }
@@ -108,7 +97,7 @@ class Attendance : AppCompatActivity() {
         spinClass.onItemSelectedListener = object : AdapterView.OnItemSelectedListener
         {
             override fun onItemSelected(parent: AdapterView<*>, v: View, position: Int, arg3: Long) {
-                val spinnerValue = parent.getItemAtPosition(position).toString()
+                spinnerValue = parent.getItemAtPosition(position).toString()
                 Toast.makeText(baseContext, "Selected Class $spinnerValue", Toast.LENGTH_SHORT).show()
                 val myRef = database.getReference(spinnerValue).child("Students")
                 executor.execute {
@@ -148,83 +137,20 @@ class Attendance : AppCompatActivity() {
 
         recordAtt.setOnClickListener(){
             val attendedStudents = Items.filter { it.attended }
+            val myRef = database.getReference(spinnerValue).child("Attendances")
+            var count = 0
             // Process the list of attended students as needed
             // For example, you can print their names:
             for (student in attendedStudents) {
+                count++
+                val myRef = database.getReference(spinnerValue).child("Students")
+                myRef.child(student.Student).child(formattedFB ).setValue("Attended")
                 Log.d("Attendance", "Attended: ${student.Student}")
             }
+
+            myRef.child(formattedFB).setValue(count.toString())
         }
 
-    }
-
-    private inner class DateAdapter : RecyclerView.Adapter<DateAdapter.DateViewHolder>() {
-
-        private var dateList = ArrayList<Date>()
-
-        fun setDateList(list: List<Date>) {
-            dateList.clear()
-            dateList.addAll(list)
-            notifyDataSetChanged()
-        }
-
-        override fun onCreateViewHolder(
-            parent: ViewGroup,
-            viewType: Int
-        ): DateViewHolder {
-            val view =
-                LayoutInflater.from(parent.context).inflate(R.layout.item_date, parent, false)
-            return DateViewHolder(view)
-        }
-        private var selectedItemPosition = RecyclerView.NO_POSITION
-        override fun onBindViewHolder(holder: DateViewHolder, position: Int) {
-            val date = dateList[position]
-            val dateFormat = SimpleDateFormat("MMM dd", Locale.getDefault())
-            val formattedDate = dateFormat.format(date)
-            holder.bind(formattedDate, position == selectedItemPosition)
-        }
-
-        override fun getItemCount(): Int {
-            return dateList.size
-        }
-
-        inner class DateViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-            private val dateTextView: TextView = itemView.findViewById(R.id.dateTextView)
-
-            fun bind(date: String, isSelected: Boolean) {
-                dateTextView.text = date
-                if (isSelected) {
-                    // Apply highlighting to the selected date
-                    itemView.setBackgroundResource(R.drawable.selected_date_item_background)
-                    dateTextView.setTextColor(ContextCompat.getColor(itemView.context,
-                        R.color.selected_date_text_color
-                    ))
-                } else {
-                    // Reset background and text color for unselected dates
-                    itemView.setBackgroundResource(0)
-                    dateTextView.setTextColor(ContextCompat.getColor(itemView.context,
-                        R.color.date_text_color
-                    ))
-                }
-
-
-                // Add click listener to handle item click
-                itemView.setOnClickListener {
-                    val clickedDate = dateList[adapterPosition]
-                    val previousSelectedPosition = selectedItemPosition
-                    selectedItemPosition = adapterPosition
-                    notifyItemChanged(previousSelectedPosition)
-                    notifyItemChanged(selectedItemPosition)
-                    // Handle the click event for the selected date
-
-                    Toast.makeText(
-                        itemView.context,
-                        "Clicked date: $clickedDate",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        }
     }
 }
 
--- app/src/main/java/uviwe/app/uviweappv1/donation.kt
+++ app/src/main/java/uviwe/app/uviweappv1/donation.kt
@@ -33,6 +33,7 @@ import java.util.concurrent.Executors
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.CheckBox
 import android.widget.PopupWindow
 import android.widget.Spinner
 import androidx.core.content.ContextCompat
@@ -51,16 +52,15 @@ import java.util.Locale
 
 //
 class donation : AppCompatActivity() {
-    private var auth: FirebaseAuth,
-    lateinit var pieChart: PieChart
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donation)
         // Initialize Firebase Auth
         auth = Firebase.auth
-        /*private lateinit*/ var auth: FirebaseAuth
-        /*private*/ var storagRef = Firebase.storage
-        //
+        val database =
+            FirebaseDatabase.getInstance("https://wil-prototype-default-rtdb.europe-west1.firebasedatabase.app/")
+        val myRef = database.getReference().child("Donations")
         //Declaring Variables
         val FirstName: TextView = findViewById(R.id.txtFirstName)
         val LastName: TextView = findViewById<EditText>(R.id.txtLastName)
@@ -68,136 +68,37 @@ class donation : AppCompatActivity() {
         val PayerEmail: TextView = findViewById<EditText>(R.id.txtPayerEmail)
         val Amount: TextView = findViewById<EditText>(R.id.txtAmount)
         val Notes: TextView = findViewById<EditText>(R.id.txtNotes)
-        val Event: TextView = findViewById<EditText>(R.id.txtEvent)
-        val AmountPaid: TextView = findViewById<EditText>(R.id.txtAmountPaid)
-        //val txtAmountPaid: TextView = findViewById<EditText>(R.id.txtAmountPaid)
         val Date: TextView = findViewById<EditText>(R.id.txtDate)
-        val Confirmation: TextView = findViewById<EditText>(R.id.cbConfirmation)
+        val Confirm: CheckBox = findViewById(R.id.cbConfirmation)
         //Button Decalre
         val Record: Button = findViewById(R.id.btnRecord)
-        //
 
-        // on below line we are initializing our
-        // variable with their ids.
-        pieChart = findViewById(R.id.pieChart)
 
-        // on below line we are setting user percent value,
-        // setting description as enabled and offset for pie chart
-        pieChart.setUsePercentValues(true)
-        pieChart.getDescription().setEnabled(false)
-        pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
-
-        // on below line we are setting drag for our pie chart
-        pieChart.setDragDecelerationFrictionCoef(0.95f)
-
-        // on below line we are setting hole
-        // and hole color for pie chart
-        pieChart.setDrawHoleEnabled(true)
-        pieChart.setHoleColor(Color.WHITE)
-
-        // on below line we are setting circle color and alpha
-        pieChart.setTransparentCircleColor(Color.WHITE)
-        pieChart.setTransparentCircleAlpha(110)
-
-        // on  below line we are setting hole radius
-        pieChart.setHoleRadius(58f)
-        pieChart.setTransparentCircleRadius(61f)
-
-        // on below line we are setting center text
-        pieChart.setDrawCenterText(true)
-
-        // on below line we are setting
-        // rotation for our pie chart
-        pieChart.setRotationAngle(0f)
-
-        // enable rotation of the pieChart by touch
-        pieChart.setRotationEnabled(true)
-        pieChart.setHighlightPerTapEnabled(true)
-
-        // on below line we are setting animation for our pie chart
-        pieChart.animateY(1400, Easing.EaseInOutQuad)
-
-        // on below line we are disabling our legend for pie chart
-        pieChart.legend.isEnabled = false
-        pieChart.setEntryLabelColor(Color.WHITE)
-        pieChart.setEntryLabelTextSize(12f)
-
-        // on below line we are creating array list and
-        // adding data to it to display in pie chart
-        val entries: ArrayList<PieEntry> = ArrayList()
-        entries.add(PieEntry(70f))
-        entries.add(PieEntry(20f))
-        entries.add(PieEntry(10f))
-
-        // on below line we are setting pie data set
-        val dataSet = PieDataSet(entries, "Donations")
-
-        // on below line we are setting icons.
-        dataSet.setDrawIcons(false)
-
-        // on below line we are setting slice for pie
-        dataSet.sliceSpace = 3f
-        dataSet.iconsOffset = MPPointF(0f, 40f)
-        dataSet.selectionShift = 5f
-
-        // add a lot of colors to list
-        val colors: ArrayList<Int> = ArrayList()
-        colors.add(resources.getColor(R.color.purple_200))
-        colors.add(resources.getColor(R.color.yellow))
-        colors.add(resources.getColor(R.color.red))
-
-        // on below line we are setting colors.
-        dataSet.colors = colors
-
-        // on below line we are setting pie data set
-        val data = PieData(dataSet)
-        data.setValueFormatter(PercentFormatter())
-        data.setValueTextSize(15f)
-        data.setValueTypeface(Typeface.DEFAULT_BOLD)
-        data.setValueTextColor(Color.WHITE)
-        pieChart.setData(data)
-
-        // undo all highlights
-        pieChart.highlightValues(null)
-
-        // loading chart
-        pieChart.invalidate()
-
-        //Firebase Code
-        //Declaring Variables
-        //Second Method
-        /*val FirstName: TextView = findViewById<EditText>(R.id.txtFirstName)}
-        val LastName: TextView = findViewById<EditText>(R.id.txtLastName)}
-        val Contact: TextView = findViewById<EditText>(R.id.txtContact)}
-        val PayerEmail: TextView = findViewById<EditText>(R.id.txtPayerEmail)}
-        val Amount: TextView = findViewById<EditText>(R.id.txtAmount)}
-        val Notes: TextView = findViewById<EditText>(R.id.txtFirstName)}
-        val Event: TextView = findViewById<EditText>(R.id.txtEvent)}
-        val AmountPaid: TextView = findViewById<EditText>(R.id.txtNotes)}
-        val Date: TextView = findViewById<EditText>(R.id.txtDate)}
-        val Confirmation: TextView = findViewById<EditText>(R.id.cbConfirmation)}*/
-
-        //NEW FIREBASE CODE IS BELOWE (Line 182 to Line 200)
-        /*
-        btnRecord.setOnClickListener()
+        Record.setOnClickListener()
         {
-            myRef.child("Donations").child(currentTime).child("Image").setValue(mapImage).addOnSuccessListener {
-                Toast.makeText(this, "Successful" , Toast.LENGTH_SHORT).show()
-            } .addOnFailureListener{ error ->
-                Toast.makeText(this, it.toString(),  Toast.LENGTH_SHORT).show()
+            if(Confirm.isActivated != false){
+                val currentTime = System.currentTimeMillis().toString()
+                myRef.child(currentTime).child("FirstName").setValue(FirstName.text.toString())
+                myRef.child(currentTime).child("LastName").setValue(LastName.text.toString())
+                myRef.child(currentTime).child("Contact").setValue(Contact.text.toString())
+                myRef.child(currentTime).child("PayerEmail").setValue(PayerEmail.text.toString())
+                myRef.child(currentTime).child("Amount").setValue(Amount.text.toString())
+                myRef.child(currentTime).child("Notes").setValue(Notes.text.toString())
+                myRef.child(currentTime).child("Date").setValue(Date.text.toString())
+                Toast.makeText(this, "Donation Saved.", Toast.LENGTH_SHORT).show()
+
+                FirstName.text = ""
+                LastName.text = ""
+                Contact.text = ""
+                PayerEmail.text = ""
+                Amount.text = ""
+                Notes.text = ""
+                Date.text = ""
+            } else {
+                Toast.makeText(this, "Please Confirm", Toast.LENGTH_SHORT).show()
             }
 
-            myRef.child("Donations").child(currentTime).child("FirstName").setValue(FirstName.text.toString())
-            myRef.child("Donations").child(currentTime).child("LastName").setValue(LastName.text.toString())
-            myRef.child("Donations").child(currentTime).child("Contact").setValue(Contact.text.toString())
-            myRef.child("Donations").child(currentTime).child("PayerEmail").setValue(PayerEmail.text.toString())
-            myRef.child("Donations").child(currentTime).child("Amount").setValue(Amount.toString())
-            myRef.child("Donations").child(currentTime).child("Notes").setValue(Notes.text.toString())
-            myRef.child("Donations").child(currentTime).child("Event").setValue(Event.text.toString())
-            myRef.child("Donations").child(currentTime).child("AmountPaid").setValue(AmountPaid.text.toString())
-            myRef.child("Donations").child(currentTime).child("Confirmation").setValue(Confirmation.text.toString())
 
-            Toast.makeText(this, "Donation Saved.", Toast.LENGTH_SHORT).show()
-        }*/
-    }//end of first bracket
+        }
+    }
 }
